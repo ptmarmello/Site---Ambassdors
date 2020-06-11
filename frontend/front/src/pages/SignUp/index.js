6aaf9610d0aadb23d8e86f5d76dee55f1c990e2c
Pedro Thiago <ptmarmello@gmail.com>
2020-06-11 17:10:43 -0300

    Second Mobile Version

--- frontend/front/src/pages/SignUp/index.js
+++ frontend/front/src/pages/SignUp/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link} from "react-router-dom";
 import {FiArrowLeft} from 'react-icons/fi';
-import './styles.css';
+import "./styles.css";
 
 import logoImg from '../../assets/logo.png';
 
